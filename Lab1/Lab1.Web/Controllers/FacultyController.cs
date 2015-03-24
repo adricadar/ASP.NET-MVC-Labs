d12a33d9f37ee09bb869e8386f8e76ff7b089e40
Adrian Cadar <cadar.adri@gmail.com>
2015-03-24 16:45:33 +0200

    Adding labs project

---
+++ Lab1/Lab1.Web/Controllers/FacultyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab1.Web.Models;
+
+namespace Lab1.Web.Controllers
+{
+    public class FacultyController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            var faculty = new Faculty
+            {
+                TeachersCount = 2,
+                StudentsCount = 10,
+                Name = "AC"
+            };
+
+            return View(faculty);
+        }
+    }
+}
