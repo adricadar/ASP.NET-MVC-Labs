4f752f0665cf2c58f324dedc1d8859dfb6d77a6f
Adrian Cadar <cadar.adri@gmail.com>
2015-03-24 16:31:59 +0200

    Lab2

---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Lab2.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Lab2.Web.Startup))]
+namespace Lab2.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
